baee35d2bae5c024f2222c08cef60882ecceb7fa
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-06-30 17:57:42 +0300

    second-commit

--- script.js
+++ script.js
@@ -57,6 +57,67 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    class createHostCard{
+        constructor(name, src, parentSelector, ...descr){
+            this.name = name;
+            this.src = src;
+            this.parent = document.querySelector(parentSelector);
+            this.descr = descr;
+        }
+
+        render(){
+            const element = document.createElement('div');
+            element.classList.add('meet-host');
+            element.innerHTML = `
+            <div class="about-host">
+                <button class="meet-your-host"><img class="right-vector" src="img/right-vector.png" alt="Done"></button>
+                <a class="meet-your-host-link" href="#">Meet your host</a>
+                <h2 class="name-host">${this.name}</h2>
+                <p class="host-descr">
+                    ${this.descr[0]}
+                </p>
+                <p class="host-descr">
+                    ${this.descr[1]}
+                </p>
+            </div>
+            <div class="container-host-img">
+                <img class="host-img" src=${this.src} alt="host-image">
+            </div>
+            `
+            this.parent.append(element);
+        }
+    }
+
+    class createReview {
+        constructor(ratingStarsCount, comments, author, parentSelector){
+            this.ratingStarsCount = ratingStarsCount;
+            this.comments = comments;
+            this.author = author;
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        render(){
+            const element = document.createElement('div');
+            const rating = document.createElement('div');
+            rating.classList.add('rating');
+            if (this.ratingStarsCount > 0 && this.ratingStarsCount < 6) {
+                for (let i = 0; i < this.ratingStarsCount; i++){
+                    rating.innerHTML += `<img class="rating-star" src="img/rating-star.png" alt="Звезда">`
+                }
+                element.classList.add('review');
+                element.append(rating);
+                    
+                element.innerHTML += `
+                    <p class="comments">${this.comments}</p>
+                    <p class="author">${this.author}</p>
+                `
+                this.parent.append(element);
+            } else {
+                console.log('the number of rating stars should be 1 <= x <= 5')
+            }
+        }
+    }
+
     new EpisodeCard(
         "img/gear.png",
         "gear",
@@ -85,5 +146,55 @@ document.addEventListener('DOMContentLoaded', () => {
         'The best microphone under $200',
         'With so many microphones on the market, how are you supposed to know what’s the best? Take a look at our top picks.',
         '.episodes-blocks'
+    ).render();
+
+    new createHostCard(
+        'Jacob Paulaner',
+        "img/host-img.jfif",
+        '.section-host .container',
+        'Jacob has a background in audio engineering, and has been podcasting since the early days.',
+        'He’s here to help you level up your game by sharing everything he’s learned along the way.'
+    ).render();
+
+    new createReview(
+        5,
+        'I can’t recommend this podcast enough',
+        'Betty Lacey',
+        '.reviews-block'
+    ).render();
+
+    new createReview(
+        5,
+        'Jacob is the best in the business',
+        'Adam Driver',
+        '.reviews-block'
+    ).render();
+
+    new createReview(
+        5,
+        'A wealth of audio knowledge',
+        'Marcus Brown',
+        '.reviews-block'
+    ).render();
+
+    new createReview(
+        5,
+        'Every episode is a gem!',
+        'Jessica Knowl',
+        '.reviews-block'
+    ).render();
+
+    new createReview(
+        5,
+        'Whoa whoa, let me take some notes!',
+        'Scott Adams',
+        '.reviews-block'
+    ).render();
+
+    new createReview(
+        5,
+        'I’ve upped my game considerably since I started listening',
+        'Steven Blast',
+        '.reviews-block'
     ).render();
 });
